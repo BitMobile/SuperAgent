3d124b038f3a5af78fc7576b16c97d8aabe54300
AleksandrKugushev <kugushew@gmail.com>
2013-10-02 19:20:57 +0400

    Add example of events
    In Events.js

---
+++ SuperAgent/solution/resource/app/Script/Events.js
@@ -0,0 +1,15 @@
+﻿//function OnLoading(screenName) {}
+
+//function OnLoad(screenName) {}
+
+//function OnWorkflowStart(name, lastStep, nextStep) {}
+
+//function OnWorkflowForward(name, lastStep, nextStep) {}
+
+//function OnWorkflowBack(name, lastStep, nextStep) {}
+
+//function OnWorkflowCommit(name, lastStep, nextStep) {}
+
+//function OnWorkflowRollback(name, lastStep, nextStep) {}
+
+//function OnWorkflowStop(name, lastStep, nextStep) {}

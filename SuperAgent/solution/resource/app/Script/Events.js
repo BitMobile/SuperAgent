2e360e55733c8d61ba3a00ce6f6c4dfa130dfb27
AleksandrKugushev <kugushew@gmail.com>
2013-11-07 12:42:53 +0400

    Add Outlet_Map

--- SuperAgent/solution/resource/app/Script/Events.js
+++ SuperAgent/solution/resource/app/Script/Events.js
@@ -1,14 +1,24 @@
-﻿function OnWorkflowStart(name) {
+﻿// ------------------------ Events ------------------------
+function OnLoad(screenName) {
+    if (screenName == "Outlet_Map.xml") {
+        var outlet = Variables["outlet"];
+        Variables["map"].AddMarker("red", outlet.Lattitude, outlet.Longitude);
+    }
+}
+
+function OnWorkflowStart(name) {
     if (name == "UnscheduledVisit" || name == "ScheduledVisit") {
         GPS.StartTracking();
     }
 }
 
-function OnWorkflowForward(name, lastStep, nextStep) { }
+//function OnWorkflowForward(name, lastStep, nextStep) { }
 
-    function OnWorkflowBack(name, lastStep, nextStep) { }
+//function OnWorkflowBack(name, lastStep, nextStep) { }
 
-    function OnWorkflowFinish(name, reason) {
-        if (name == "UnscheduledVisit" || name == "ScheduledVisit")
-            GPS.StopTracking();
+function OnWorkflowFinish(name, reason) {
+    if (name == "UnscheduledVisit" || name == "ScheduledVisit")
+        GPS.StopTracking();
+}
+
     }

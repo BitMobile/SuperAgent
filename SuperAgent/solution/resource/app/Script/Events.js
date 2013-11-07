025158afe8db51e5df3b5db5d86e23e1c6dac636
MaryGorshkova <mint.mushrooom@gmail.com>
2013-11-07 17:44:54 +0400

    hot fix

--- SuperAgent/solution/resource/app/Script/Events.js
+++ SuperAgent/solution/resource/app/Script/Events.js
@@ -20,38 +20,11 @@ function OnWorkflowStart(name) {
 //function OnWorkflowForward(name, lastStep, nextStep) { }
 
 function OnWorkflowBack(name, lastStep, nextStep) {
-<<<<<<< HEAD
+
     if (lastStep == "PriceLists" && nextStep == "Order")
         ReviseSKUs();
-=======
-    if (lastStep == "PriceLists" && nextStep == "Order") {
 
-        //revising SKUs
-        var query = new Query();
-        query.AddParameter("orderId", Variables["workflow"]["order"].Id);
-        query.Text = "select * from Document.Order_SKUs where Ref==@orderId limit 100";
-        var SKUs = query.Execute();
-        var s = SKUs.Count();
 
-        if (parseInt(SKUs.Count()) != parseInt(0))
-            Dialog.Message("SKU list will be revised");
-
-        for (var k in SKUs) {
-            var query2 = new Query();
-            query2.AddParameter("PLid", Variables["workflow"]["order"].PriceList);
-            query2.AddParameter("sku", k.SKU);
-            query2.Text = "select single(*) from Document.PriceList_Prices where Ref==@PLid && SKU==@sku";
-            var pricelistItem = query2.Execute();
-            if (pricelistItem == null)
-                DB.Current.Document.Order_SKUs.Delete(k);
-            else {
-                k.Price = pricelistItem.Price;
-                k.Total = (k.Discount / 100 + 1) * k.Price;
-                k.Amount = k.Qty * k.Total;
-            }
-        }
-    }
->>>>>>> 7b48e282e5b4d1d3060eefecb81dbf2790d9a13f
 }
 
 function OnWorkflowFinish(name, reason) {

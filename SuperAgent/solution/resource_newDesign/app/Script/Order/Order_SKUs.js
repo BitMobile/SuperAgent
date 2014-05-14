0dda577b0b4f7becd5c55ba2de73bdd8e7158f4c
MaryGorshkova <mint.mushrooom@gmail.com>
2014-05-14 18:32:05 +0400

    Filters remake, bugfix

--- SuperAgent/solution/resource_newDesign/app/Script/Order/Order_SKUs.js
+++ SuperAgent/solution/resource_newDesign/app/Script/Order/Order_SKUs.js
@@ -3,7 +3,7 @@
 function GetSKUAndGroups(searchText, priceList) {
     if (String.IsNullOrEmpty(searchText)) {
         var query = new Query();
-        query.Text = "SELECT S.Id, S.Description, PL.Price, S.CommonStock, G.Description AS GroupDescription FROM Catalog_SKU S JOIN Catalog_SKUGroup G ON G.Id = S.Owner JOIN Document_PriceList_Prices PL ON PL.SKU = S.Id WHERE S.CommonStock>0 AND PL.Ref = @Ref ORDER BY G.Description, S.Description LIMIT 100";
+        query.Text = "SELECT S.Id, S.Description, PL.Price, S.CommonStock, G.Description AS GroupDescription, CB.Description AS Brand FROM Catalog_SKU S JOIN Catalog_SKUGroup G ON G.Id = S.Owner JOIN Document_PriceList_Prices PL ON PL.SKU = S.Id JOIN Catalog_Brands CB ON CB.Id=S.Brand WHERE S.CommonStock>0 AND PL.Ref = @Ref ORDER BY G.Description, S.Description LIMIT 100";
         query.AddParameter("Ref", priceList);
         return query.Execute();
     }
@@ -44,6 +44,14 @@ function CheckFilterAndForward() {
     Workflow.Forward([]);
 }
 
+function CheckFilter(filterName) {
+    if (Variables.Exists(filterName)) {
+        var t = Variables[filterName];
+        if (parseInt(t.Count()) == parseInt(0))
+            Variables.Remove(filterName);
+    }
+}
+
 function GetLeftFilterStyle(val) {
     if (Variables["filterType"] == val)
         return "mode_left_button_on";
@@ -67,12 +75,7 @@ function ChangeFilterAndRefresh(type) {
 
 function GetParentGroups() {
     //return DB.Current.Catalog.SKUGroup.SelectBy("Parent", groups, "<>").OrderBy("Description");
-    var q = new Query("SELECT Id, Description, Parent FROM Catalog_SKUGroup WHERE Parent IS NULL ORDER BY S.Description");
-    Dialog.Debug(q.ExecuteCount());
-    var c = q.Execute();
-    while (c.Next()){
-        Dialog.Debug(c.Parent);
-    }
+    var q = new Query("SELECT Id, Description, Parent FROM Catalog_SKUGroup WHERE Parent IS NULL ORDER BY Description");   
     return q.Execute();
 }
 
@@ -105,21 +108,20 @@ function AddFilterAndRefresh(item, filterName) { //refactoring is needed after p
         nCollection.push(i);
     }
 
-    var isInCollection = IsInCollection(item.Id, f);
-    if (isInCollection) {
-        nCollection = DeleteFromCollection(item.Id, nCollection);
+    if (IsInCollection(item, f)) {
+        nCollection = DeleteFromCollection(item, nCollection);
         if (item.IsFolder) {
             var chld = GetChildren(item);
             for (var i in chld)
-                nCollection = DeleteFromCollection(i.Id, nCollection);
+                nCollection = DeleteFromCollection(i, nCollection);
         }
     }
     else {
-        nCollection.push(item.Id);
+        nCollection.push(item);
         if (item.IsFolder) {
             var chld = GetChildren(item);
             for (var i in chld)
-                nCollection.push(i.Id);
+                nCollection.push(i);
         }
     }
 
@@ -140,6 +142,6 @@ function FilterIsSet(itemId, filterName) {
 function GetBrands() {
     //var sku = DB.Current.Catalog.SKU.Select().Distinct("Brand");
     //return DB.Current.Catalog.Brands.SelectBy("Id", sku);
-    var q = new Query("SELECT B.Id, B.Description FROM Catalog_Brands B JOIN Catalog_SKU S ON B.Id=S.Brand ORDER BY B.Description");
+    var q = new Query("SELECT DISTINCT B.Id, B.Description FROM Catalog_Brands B JOIN Catalog_SKU S ON B.Id=S.Brand ORDER BY B.Description");
     return q.Execute();
 }

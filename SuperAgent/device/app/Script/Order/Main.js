a56dcb36d60ca55e4396808561b141a54428021b
MaryGorshkova <mint.mushrooom@gmail.com>
2014-06-09 16:35:35 +0400

    "Canceled" mark at OrderList add

--- SuperAgent/device/app/Script/Order/Main.js
+++ SuperAgent/device/app/Script/Order/Main.js
@@ -9,8 +9,14 @@ function GetOrderList() {
 	// [searchText]).OrderBy("Date", true).Top(100);
 	// }
 	var q = new Query(
-			"SELECT DO.Id, DO.Outlet, strftime('%d/%m/%Y', DO.Date) AS Date, DO.Number, CO.Description AS OutletDescription FROM Document_Order DO JOIN Catalog_Outlet CO ON DO.Outlet=CO.Id ORDER BY DO.Date DESC LIMIT 100");
+			"SELECT DO.Id, DO.Outlet, strftime('%d/%m/%Y', DO.Date) AS Date, DO.Number, CO.Description AS OutletDescription, DO.Status FROM Document_Order DO JOIN Catalog_Outlet CO ON DO.Outlet=CO.Id ORDER BY DO.Date DESC LIMIT 100");
 	return q.Execute();
+}
+
+function OrderCanceled(status){
+	if (status.ToString() == (DB.Current.Constant.OrderSatus.Canceled).ToString())
+		return true;	
+	return false;
 }
 
 function AssignNumberIfNotExist(number) {

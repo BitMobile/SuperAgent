3b795dd26c65536099e278574e559d7761ea8f76
MaryGorshkova <mint.mushrooom@gmail.com>
2014-12-22 11:48:20 +0300

    write answer history bugfix

--- SuperAgent/device/app/Script/Visits/Total.js
+++ SuperAgent/device/app/Script/Visits/Total.js
@@ -222,9 +222,7 @@ function FillQuestionnaires() {
 			var q2 = new Query("SELECT A.Id FROM Catalog_Outlet_AnsweredQuestions A " +
 					" JOIN Document_Questionnaire_Schedule S ON A.Questionaire=S.Ref " +
 					" WHERE A.Questionaire=@questionnaire " +
-					" AND A.Ref=@outlet AND A.Question=@question AND A.SKU=@sku " +
-					" AND (date('now','start of day')>= DATE(S.BeginAnswerPeriod) AND DATE(S.EndAnswerPeriod) >= date('now','start of day')) " +
-					" AND (DATE(A.AnswerDate)>= DATE(S.BeginAnswerPeriod) AND DATE(S.EndAnswerPeriod) >= DATE(A.AnswerDate))");
+					" AND A.Ref=@outlet AND A.Question=@question AND A.SKU=@sku");
 			q2.AddParameter("questionnaire", res.Questionnaire);
 			q2.AddParameter("outlet", $.workflow.outlet);
 			q2.AddParameter("sku", resSKU);

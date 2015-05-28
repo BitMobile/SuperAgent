a18020f00cf284d7062c9eff5fe16bdfa89e8e78
MaryGorshkova <mint.mushrooom@gmail.com>
2015-05-28 15:29:55 +0300

    SUPA-1373 design complete

--- SuperAgent/device/app/Script/About/Main.js
+++ SuperAgent/device/app/Script/About/Main.js
@@ -1,6 +1,15 @@
 
 function SendLog(){
+	// $.sendButton.Visible = false;
+	// $.dataSyncLayout.Visible = true;
+	// $.dataSyncIndicator.Start();
+
 	var result = Application.SendDatabase();
+
+	// $.sendButton.Visible = true;
+	// $.dataSyncLayout.Visible = false;
+	// $.dataSyncIndicator.Stop();	
+
 	if (result)
 		Dialog.Message(Translate["#success#"]);
 	else

361dcf7442fe67f52ce19217e21e93f33eff77d3
MaryGorshkova <mint.mushrooom@gmail.com>
2015-06-01 12:18:51 +0300

    symbol bugfix

--- SuperAgent/device/app/Script/Visits/Questions.js
+++ SuperAgent/device/app/Script/Visits/Questions.js
@@ -340,7 +340,7 @@ function ChooseBool(entity, attribute, control, func, title) {
 	else
 		var startKey = entity[attribute];
 
-	var listChoice = [[ "—", "-" ], [Translate["#YES#"], Translate["#YES#"]], [Translate["#NO#"], Translate["#NO#"]]];
+	var listChoice = [[ "—", "-" ], [Translate["#YES#"], Translate["#YES#"]], [Translate["#NO#"], Translate["#NO#"]]];
 	if (func == null)
 		func = CallBack;
 	Dialog.Choose(title, listChoice, startKey, func, [entity, attribute, control]);

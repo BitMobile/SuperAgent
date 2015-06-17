5e6298dfef891bdb7f94050519d4617af9aa5069
13470 <gsouth@gmail.com>
2015-06-17 12:37:36 +0300

    SUPA-1435 Fixes

--- SuperAgent/device/app/Script/Receivables/Main.js
+++ SuperAgent/device/app/Script/Receivables/Main.js
@@ -110,6 +110,7 @@ function CreateEncashmentItem(encashment, receivableDoc) {
 
 function SpreadEncasmentAndRefresh(encashment, outlet, receivables) {
 
+	SaveSum();
 	receivables = GetReceivables(outlet);
 
 	var sumToSpread = encashment.EncashmentAmount;
@@ -193,8 +194,8 @@ function SaveSum(control) {
 	}
 	else {
 		enc.EncashmentAmount = ToDecimal($.encAmount.Text);
+		$.encAmount.Text = enc.EncashmentAmount;
 		enc.Save();
-		DoRefresh();
 	}
 }
 

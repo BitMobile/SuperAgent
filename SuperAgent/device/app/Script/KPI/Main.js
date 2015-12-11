f9e85c1437f2eda882f5c28c72f94b7624bda35f
unknown <mkgorshkova@w1551.BT>
2015-12-11 16:49:23 +0300

    kpi fix

--- SuperAgent/device/app/Script/KPI/Main.js
+++ SuperAgent/device/app/Script/KPI/Main.js
@@ -1,5 +1,5 @@
 function PlanVisitsKPI(){
-	var q = new Query("SELECT COUNT(DISTINCT VP.Outlet) " +
+	var q = new Query("SELECT COUNT(VP.Outlet) " +
 		" FROM Document_Visit V " +
 		" JOIN Document_VisitPlan_Outlets VP ON VP.Outlet=V.Outlet " +
 		" JOIN Document_VisitPlan DV ON VP.Ref = DV.Id " +
@@ -7,7 +7,8 @@ function PlanVisitsKPI(){
 	q.AddParameter("emptyRef", DB.EmptyRef("Document_VisitPlan"));
 	var executed = q.ExecuteScalar();		
 
-	var q = new Query("SELECT COUNT(DISTINCT VPO.Outlet) FROM Document_VisitPlan_Outlets VPO " +
+	var q = new Query("SELECT COUNT(VPO.Outlet) " +
+		" FROM Document_VisitPlan_Outlets VPO " +
 		" JOIN Document_VisitPlan DP ON VPO.Ref = DP.Id " +
 		" JOIN Catalog_Outlet O ON VPO.Outlet=O.Id " +
 		" JOIN Catalog_OutletsStatusesSettings OSS ON O.OutletStatus=OSS.Status AND OSS.ShowOutletInMA=1 AND OSS.DoVisitInMA=1 " +

76c0eddcdb242bb84416d7b9b7a01cc82da6b952
unknown <mkgorshkova@w1551.BT>
2015-12-14 15:55:32 +0300

    SUPA-1834 kpi query fix

--- SuperAgent/device/app/Script/KPI/Main.js
+++ SuperAgent/device/app/Script/KPI/Main.js
@@ -1,7 +1,7 @@
 function PlanVisitsKPI(){
 	var q = new Query("SELECT COUNT(VP.Outlet) " +
 		" FROM Document_Visit V " +
-		" JOIN Document_VisitPlan_Outlets VP ON VP.Outlet=V.Outlet " +
+		" JOIN Document_VisitPlan_Outlets VP ON VP.Outlet=V.Outlet AND DATE(V.Date)=DATE(VP.Date) " +
 		" JOIN Document_VisitPlan DV ON VP.Ref = DV.Id " +
 		" WHERE DATE(V.Date) >= DATE('now', 'start of month', 'localtime') AND V.Plan <> @emptyRef ");
 	q.AddParameter("emptyRef", DB.EmptyRef("Document_VisitPlan"));
@@ -12,7 +12,7 @@ function PlanVisitsKPI(){
 		" JOIN Document_VisitPlan DP ON VPO.Ref = DP.Id " +
 		" JOIN Catalog_Outlet O ON VPO.Outlet=O.Id " +
 		" JOIN Catalog_OutletsStatusesSettings OSS ON O.OutletStatus=OSS.Status AND OSS.ShowOutletInMA=1 AND OSS.DoVisitInMA=1 " +
-		" WHERE DATE(VPO.Date)>=DATE('now', 'start of month', 'localtime') AND NOT OSS.Status IS NULL");
+		" WHERE DATE(VPO.Date)>=DATE('now', 'start of month', 'localtime') AND DATE(VPO.Date)<=DATE('now', 'localtime') AND NOT OSS.Status IS NULL");
 	var planned = q.ExecuteScalar();
 
 	return parseFloat(planned)==parseFloat(0) ? "0" : String.Format("{0:F0}", (executed * 100 / planned) || 0);

343a046bbca4ce65f917536245c63ebe2c213789
LieOff <IIObmanov@1cbit.ru>
2016-09-19 18:13:44 +0300

    ResponsMerop
    ResponsMerop

--- SuperAgent/device/app/Script/Outlets/Main.js
+++ SuperAgent/device/app/Script/Outlets/Main.js
@@ -864,6 +864,7 @@ function CreateVisitIfNotExists(userRef, visit, planVisit) {
 		meropFact.ContactPerson = $.MeropCur.ContactPerson;
 		var q1 = new Query("Select Id From Enum_TypeMeropPlan Where Name = 'UnPlan'");
 		meropFact.TypeMerop = q1.ExecuteScalar();
+		meropFact.SR = $.common.UserRef;
 		meropFact.TypeMeropr = $.MeropCur.TypeMeropr;
 		meropFact.DO = $.workflow.outlet;
 		if ($.MeropCur.WithRuk == null) {

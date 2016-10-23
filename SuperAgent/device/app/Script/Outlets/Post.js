637af9296f6696a38a07e28a532b7fd65ec0b494
zemskix <denis@zemskix.com>
2016-10-24 00:22:16 +0300

    INT-1421

---
+++ SuperAgent/device/app/Script/Outlets/Post.js
@@ -0,0 +1,88 @@
+﻿var outlet;
+var contact;
+var contactRef;
+var sear;
+var linkcont;
+
+function OnLoading(){
+
+	var qw = new Query("SELECT COUNT(Id) FROM Catalog_ContactPersons WHERE Id = @outlet")
+	qw.AddParameter("outlet", $.param1);
+
+	contactRef = $.param1;
+	var contactsCount1 = qw.ExecuteScalar();
+	if(contactsCount1>0) {
+	linkcont = $.param1;
+	Console.WriteLine("true one");
+	contact = true;
+	}else{
+		contact = false;
+		sear=contactRef;
+		Console.WriteLine("false one");
+}
+
+}
+
+function OnLoad(){
+
+$.edtSearch.Text=sear;
+
+}
+
+
+function BackCon(){
+		sear="";
+		Workflow.Back();
+}
+
+function SaveAndBack(entity, owner, contactг) {
+
+		 	var en = linkcont.GetObject();
+		 	en.Position = owner;
+		 	en.Save(true);
+			sear="";
+			Workflow.Back();
+
+}
+
+function HasContacts(outlet){
+
+	hasOutletContacts = HasOutletContacts();
+
+	return hasOutletContacts;
+}
+
+
+function HasOutletContacts() {
+
+if(contact == true) {
+	Console.WriteLine("true");
+	var q = new Query("SELECT COUNT(Id) FROM Catalog_ContacntDolzh ")
+	Console.WriteLine("true!");
+}else{
+	var q = new Query("SELECT COUNT(Id) FROM Catalog_ContacntDolzh WHERE Description Like @outlet")
+	q.AddParameter("outlet", "%"+contactRef+"%");
+	Console.WriteLine("false!");
+		Console.WriteLine(contactRef);
+}
+
+	var contactsCount = q.ExecuteScalar();
+	return contactsCount>0;
+
+}
+
+
+function GetOutletContacts(outlet) {
+
+if(contact == true) {
+	var q = new Query("Select Id,Description From Catalog_ContacntDolzh  Order By Description");
+
+}else{
+	
+	var q = new Query("Select Id,Description From Catalog_ContacntDolzh WHERE Description Like @outlet Order By Description");
+	q.AddParameter("outlet", "%"+contactRef+"%");
+
+}
+
+		return q.Execute();
+}

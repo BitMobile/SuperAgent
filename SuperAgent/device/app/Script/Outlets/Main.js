404234737634effbaeee09149cc12987819ff1a0
ivtutukov <ivtutukov@1cbit.ru>
2016-11-25 18:22:21 +0300

    int-1602

--- SuperAgent/device/app/Script/Outlets/Main.js
+++ SuperAgent/device/app/Script/Outlets/Main.js
@@ -686,28 +686,31 @@ function SetLocation(control, outlet) {
 }
 
 function CoordsChecked(visit) {
-
-	if (parseInt(GPS.Accuracy)<parseInt(1000)){
-		var location = GPS.CurrentLocation;
-		var llocal = parseInt($.sessionConst.RadiusDeviation);
-		var messageImpossibleOutletCoords = Translate["#impossibleOutletCoords#"];
-		if (parseInt(GPS.Accuracy) > parseInt(llocal)) {
-			messageImpossibleOutletCoords = Translate["#impossibleGPSCoords#"];
-			llocal = parseInt(GPS.Accuracy);
-		}
-	// проверка на местополжение торговой точки+радиус отклонения = текущее местоположение
-		var outlet = outlet.GetObject();
-		if (parseInt(llocal) != parseInt(0)) {
-			if ((parseInt(outlet.Lattitude) != parseInt(0)) || (parseInt(outlet.Longitude) != parseInt(0))) {
-				var CurRadiusDeviation = CoordCheckOutletAndActuality(location, outlet);
-				if (parseInt(llocal) < parseInt(CurRadiusDeviation)) {
-					var Impos = parseInt(CurRadiusDeviation)- parseInt(llocal);
-					Dialog.Message(messageImpossibleOutletCoords + Impos + " " + Translate["#metr#"]);
-					return false;
+	var location = GPS.CurrentLocation;
+	var llocal = parseInt($.sessionConst.RadiusDeviation);
+	var messageImpossibleOutletCoords = Translate["#impossibleOutletCoords#"];
+	Dialog.Debug(GPS.Accuracy);
+	if (!(IsNullOrEmpty(GPS.Accuracy))){
+		if (parseInt(GPS.Accuracy)<=parseInt(1000)){
+			if (parseInt(GPS.Accuracy) > parseInt(llocal)) {
+				messageImpossibleOutletCoords = Translate["#impossibleGPSCoords#"];
+				llocal = parseInt(GPS.Accuracy);
+			}
+		// проверка на местополжение торговой точки+радиус отклонения = текущее местоположение
+			var outlet = outlet.GetObject();
+			if (parseInt(llocal) != parseInt(0)) {
+				if ((parseInt(outlet.Lattitude) != parseInt(0)) || (parseInt(outlet.Longitude) != parseInt(0))) {
+					var CurRadiusDeviation = CoordCheckOutletAndActuality(location, outlet);
+					if (parseInt(llocal) < parseInt(CurRadiusDeviation)) {
+						var ImposMeter = parseInt(CurRadiusDeviation) - parseInt(llocal);
+						Dialog.Message(messageImpossibleOutletCoords + " " + Impos + " " + Translate["#meter#"]);
+						return false;
+					}
 				}
 			}
 		}
 	}
+
 //
 	if (ActualLocation(location) && DateAddTru == false) {
 		var visitObj = visit.GetObject();
